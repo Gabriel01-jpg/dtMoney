366a7dff3f552132959736e51a482cd77dd0af34
Gabriel <gabriellima170801@gmail.com>
2021-09-29 15:14:48 -0300

    Finalizada

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,103 @@
+import { FormEvent, useState } from "react";
+import Modal from 'react-modal';
+import {useTransactions} from "../../hooks/useTransactions";
+
+
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+Modal.setAppElement('#root')
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+    const { createTransaction } = useTransactions()
+
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState('')
+    const [type, setType] = useState('deposit');
+
+    // um dos patterns, utilizar handle no nome da funcao quando simboliza a ação de um usuário.
+    async function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault();
+
+        await createTransaction({
+            title,
+            amount,
+            category,
+            type
+        })
+
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit')
+        onRequestClose();
+    }
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container onSubmit={handleCreateNewTransaction}>
+                <h2>Cadastrar transação</h2>
+                <input
+                placeholder="Título"
+                value={title}
+                onChange={event => setTitle(event.target.value)}
+                />
+                <input
+                    type="number"
+                placeholder="Valor"
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
+                />
+                <TransactionTypeContainer >
+                    <RadioBox
+                        type="button"
+                        onClick={() => { setType('deposit') }}
+                        isActive={type === 'deposit'}
+                        activeColor = 'green'
+                    >
+                        <img src={incomeImg} alt="Entrada"/>
+                        <span>Entrada</span>
+                    </RadioBox>
+                    <RadioBox
+                        type="button"
+                        onClick={() => { setType('withdraw') }}
+                        isActive={type === 'withdraw'}
+                        activeColor = 'red'
+                    >
+                        <img src={outcomeImg} alt="Saída"/>
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+                <input
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}
+                />
+                <button type="submit">
+                    Cadastrar
+                </button>
+                <button
+                    className="react-modal-close"
+                    type="button"
+                    onClick={onRequestClose}
+                >
+                    <img src={closeImg} alt="Fechar modal" />
+                </button>
+            </Container>
+        </Modal>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createServer, Model } from 'miragejs';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+createServer({
+    models: {
+        transaction: Model,
+    },
+    seeds(server){
+        server.db.loadData({
+            transactions: [{
+                id: 1,
+                title: 'Freelancer de website',
+                type: 'deposit',
+                category: 'Dev',
+                amount: 600,
+                createdAt: new Date('2021-02-12 09:00:00'),
+            },{
+                id: 2,
+                title: 'Celular',
+                type: 'withdraw',
+                category: 'Eletrônico',
+                amount: 1200,
+                createdAt: new Date('2021-02-15 12:54:00'),
+            }]
+        })
+    },
+
+    routes(){
+        this.namespace = 'api';
+
+        this.get('/transactions', () => {
+            return this.schema.all('transaction')
+        })
+
+        this.post('/transactions', (schema, request) => {
+            const data = JSON.parse(request.requestBody)
+
+            return schema.create('transaction', data)
+        })
+    }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +48,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
